53c420e8cd023786bcec38cc0816b4410d521696
varunaditya27 <varun.paparajugari@gmail.com>
2025-09-18 15:34:17 +0530

    Enhance Ball component with Trail effect and update Level component materials for improved visuals

--- src/components/game/Ball.tsx
+++ src/components/game/Ball.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Sphere } from '@react-three/drei';
+import { Sphere, Trail } from '@react-three/drei';
 
 interface BallProps {
   position: [number, number, number];
@@ -7,8 +7,12 @@ interface BallProps {
 
 export default function Ball({ position }: BallProps) {
   return (
-    <Sphere args={[0.2, 32, 32]} position={position}>
-      <meshStandardMaterial color="white" />
-    </Sphere>
+    <group>
+      <Trail width={2} color="#ffcc00" length={0.4} decay={0.9} attenuation={(t) => t}>
+        <Sphere args={[0.2, 32, 32]} position={position}>
+          <meshPhysicalMaterial color="#ffffff" roughness={0.2} metalness={0.1} clearcoat={0.7} />
+        </Sphere>
+      </Trail>
+    </group>
   );
 }
